c346fd74fc0804de172a71089e978426cdd034b1
Mantas Petrauskas <mantas.petrauskas96@gmail.com>
2018-11-25 20:32:58 +0200

    Added Facebook login

--- src/main.js
+++ src/main.js
@@ -1,10 +1,20 @@
 import Vue from 'vue';
+import axios from 'axios';
 import App from './App.vue';
 import router from './router';
-import store from './store';
+import store from './store/index';
 import './registerServiceWorker';
 
 Vue.config.productionTip = false;
+axios.defaults.baseURL = process.env.VUE_APP_API_ENDPOINT;
+axios.interceptors.request.use((config) => {
+  const editedConfig = config;
+  if (store.getters.accessToken) {
+    editedConfig.headers.common['x-auth-token'] = `Bearer ${store.getters.accessToken}`;
+  }
+  return editedConfig;
+});
+Vue.prototype.$http = axios;
 
 new Vue({
   router,
---
+++ src/store/auth.js
@@ -0,0 +1,79 @@
+/* eslint-disable no-shadow,consistent-return,prefer-promise-reject-errors,no-param-reassign,
+no-undef */
+import axios from 'axios';
+
+const state = {
+  accessToken: null,
+  tokenExpiresAt: null,
+  currentUser: null,
+};
+
+const getters = {
+  currentUser(state) {
+    return state.currentUser;
+  },
+
+  accessToken(state) {
+    return state.accessToken;
+  },
+
+  isAuthenticated(state) {
+    const now = new Date();
+    return state.accessToken
+      && state.tokenExpiresAt
+      && now.getTime() < state.tokenExpiresAt.getTime();
+  },
+};
+
+const mutations = {
+  setAccessToken(state, payload) {
+    state.accessToken = payload;
+  },
+};
+
+const actions = {
+  loginWithFacebook({ commit }) {
+    return new Promise((resolve, reject) => {
+      FB.login((result) => {
+        if (result.authResponse) {
+          return axios.post('api/auth/facebook', { access_token: result.authResponse.accessToken })
+            .then((res) => {
+              // TODO: needs work
+              const token = res.headers.get('x-auth-token');
+              if (token) {
+                commit('setAccessToken', token);
+                localStorage.setItem('accessToken', token);
+              }
+              resolve(res);
+            })
+            .catch(() => reject());
+        }
+        reject();
+      }, { scope: 'public_profile,email' });
+    });
+  },
+
+  logout({ commit }) {
+    commit('setAccessToken', null);
+    localStorage.removeItem('accessToken');
+  },
+
+  isLoggedIn() {
+    return new Promise((resolve, reject) => {
+      this.getCurrentUser().then(() => resolve(true)).catch(() => reject(false));
+    });
+  },
+
+  getCurrentUser() {
+    return new Promise((resolve, reject) => axios.get('api/auth/me').then((res) => {
+      resolve(res);
+    }).catch(() => reject()));
+  },
+};
+
+export default {
+  state,
+  getters,
+  mutations,
+  actions,
+};
